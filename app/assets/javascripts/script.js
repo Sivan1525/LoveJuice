2b8002d51f90b02d113b80b0e8c1e90362d59f08
Sivanand <sivanand@sivanand-dell.(none)>
2013-07-24 09:34:58 +0530

    my initial commit

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,92 @@
+/**
+ *
+ * Pinterest-like script - a series of tutorials
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2012, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+
+function fileSelectHandler() {
+    // get selected file
+    var oFile = $('#image_file')[0].files[0];
+
+    // html5 file upload
+    var formData = new FormData($('#upload_form')[0]);
+    $.ajax({
+        url: 'upload.php', //server script to process data
+        type: 'POST',
+        // ajax events
+        beforeSend: function() {
+        },
+        success: function(e) {
+            $('#upload_result').html('Thank you for your photo').show();
+
+            setTimeout(function() {
+                $("#upload_result").hide().empty();
+            }, 4000);
+        },
+        error: function(e) {
+            $('#upload_result').html('Error while processing uploaded image');
+        },
+        // form data
+        data: formData,
+        // options to tell JQuery not to process data or worry about content-type
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
+$(document).ready(function(){
+
+    // file field change handler
+    $('#image_file').change(function(){
+        var file = this.files[0];
+        name = file.name;
+        size = file.size;
+        type = file.type;
+
+        // extra validation
+        if (name && size)  {
+            if (! file.type.match('image.*')) {
+                alert("Select image please");
+            } else {
+                fileSelectHandler();
+            }
+        }
+    });
+
+    // masonry initialization
+    $('.main_container').masonry({
+        // options
+        itemSelector : '.pin',
+        isAnimated: true,
+        isFitWidth: true
+    });
+
+    // onclick event handler (for comments)
+    $('.comment_tr').click(function () {
+        $(this).toggleClass('disabled');
+        $(this).parent().parent().parent().find('form').slideToggle(250, function () {
+            $('.main_container').masonry();
+        });
+    }); 
+
+    $('.ajax').colorbox({
+        onOpen:function(){
+        },
+        onLoad:function(){
+        },
+        onComplete:function(){
+            $(this).colorbox.resize();
+        },
+        onCleanup:function(){
+        },
+        onClosed:function(){
+        }
+    });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovejuice::Application.config.secret_token = '1eb03e9eb6cd7b257356d6a5d777d62ca0985cb7430e5baa21f395a4dfb7d7ea1c919f8457374400ba7e9d687b3b26c26d2160436d44a9c800ed3094e8b22a20'
